4a2bef7833478309492049d5bbae2911cf844675
Björn Strömberg <bjorn.stromberg@codic.se>
2021-03-23 13:29:24 +0100

    Revert "Töm configureservices (Dålig commit som jag ångrar)"
    This reverts commit b010ce110d814f295a9968e6b127c81905c87ff0.

--- AspNetCore_MVC_Demo/Startup.cs
+++ AspNetCore_MVC_Demo/Startup.cs
@@ -25,6 +25,11 @@ namespace AspNetCore_MVC_Demo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+
+            services.AddDbContext<TodoDbContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("TodoDbContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
